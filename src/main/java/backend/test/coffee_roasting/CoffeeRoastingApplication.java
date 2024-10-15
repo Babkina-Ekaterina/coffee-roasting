8b6d83e9cf709a07386ea7e6b680838de1a3836e
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-10-15 09:50:48 +0300

    Initialize project

---
+++ src/main/java/backend/test/coffee_roasting/CoffeeRoastingApplication.java
@@ -0,0 +1,13 @@
+package backend.test.coffee_roasting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeRoastingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeRoastingApplication.class, args);
+	}
+
+}
