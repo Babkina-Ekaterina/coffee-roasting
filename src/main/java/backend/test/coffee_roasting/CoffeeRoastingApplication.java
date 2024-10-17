341d2cec1ca489f48967a7e37fca72f6eeee8c79
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-10-17 13:05:12 +0300

    Add rest controllers and swagger

--- src/main/java/backend/test/coffee_roasting/CoffeeRoastingApplication.java
+++ src/main/java/backend/test/coffee_roasting/CoffeeRoastingApplication.java
@@ -1,11 +1,14 @@
 package backend.test.coffee_roasting;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Coffee roasting microservice", version = "1.0.0",
+		description = "Microservice for managing coffee roasting "))
 public class CoffeeRoastingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeRoastingApplication.class, args);
 	}
