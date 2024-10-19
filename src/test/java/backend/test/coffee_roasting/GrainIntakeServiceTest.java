3ecdf4fb56cdb5835808c5a9692031b85a3899f8
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-10-20 00:59:53 +0300

    Add roasting outtake test

--- src/test/java/backend/test/coffee_roasting/GrainIntakeServiceTest.java
+++ src/test/java/backend/test/coffee_roasting/GrainIntakeServiceTest.java
@@ -20,7 +20,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class GrainIntakeServiceTests {
+public class GrainIntakeServiceTest {
     @Mock
     private GrainIntakeRepository grainIntakeRepository;
 
